97e6406abb5d6a8a7e7797dfee01afe86d7ea954
Badrinath Danave <badrinathdanave@gmail.com>
2023-04-14 23:18:55 +0530

    added feign client and enablefiegn client for application

--- src/main/java/com/springboot/cloud/feign/SpringbootCloudFeignApplication.java
+++ src/main/java/com/springboot/cloud/feign/SpringbootCloudFeignApplication.java
@@ -1,13 +1,31 @@
 package com.springboot.cloud.feign;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.cloud.feign.client.UserClient;
+import com.springboot.cloud.feign.dto.UserResponse;
 
 @SpringBootApplication
+@RestController
+@EnableFeignClients
 public class SpringbootCloudFeignApplication {
 
+	@Autowired
+	private UserClient userClient;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCloudFeignApplication.class, args);
 	}
 
+	@GetMapping("/findalluser")
+	public List<UserResponse> getAllUsers(){
+		return userClient.getUsers();
+	}
 }
