95d1659036ebc5bb12a9afe6283693a2c2a6d66a
Badrinath Danave <badrinathdanave@gmail.com>
2023-04-14 22:42:33 +0530

    initial commit

---
+++ src/test/java/com/springboot/cloud/feign/SpringbootCloudFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cloud.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
